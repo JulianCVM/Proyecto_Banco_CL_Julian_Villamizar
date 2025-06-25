164d97d1e3de59f72390642de25dea6e7f3a0d1e
Julián Camilo Villamizar Montañez <111820807+JulianCVM@users.noreply.github.com>
2025-06-25 07:28:33 -0500

    feat: :card_file_box: INSERT tipo_cliente
    Se insertan los 4 tipos de clientes

--- dml.sql
+++ dml.sql
@@ -1,3 +1,9 @@
 -- insertar los datos (hasta aqui llego mi salud mental)
 
 
+
+INSERT INTO tipo_cliente(activo,codigo,descripcion,nombre) VALUES
+(TRUE, 'REG', 'Cliente regular, no tiene beneficios especiales.', 'REGULAR'),
+(TRUE, 'EST', 'Cliente estandar, tiene beneficios basicos.', 'ESTANDAR'),
+(TRUE, 'PREF', 'Cliente preferencial, tiene beneficios preferenciales.', 'PREFERENCIAL'),
+(TRUE, 'VIP', 'Cliente regular, tiene beneficios especiales y atencion personalizada.', 'VIP');
