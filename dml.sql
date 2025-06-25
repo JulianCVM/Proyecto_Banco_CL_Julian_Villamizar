81fcc67e364e691edc9d37e8d48b098f8723394a
Julián Camilo Villamizar Montañez <111820807+JulianCVM@users.noreply.github.com>
2025-06-25 07:35:39 -0500

    feat: :card_file_box: INSERT tipo_cuenta

--- dml.sql
+++ dml.sql
@@ -2,8 +2,19 @@
 
 
 
-INSERT INTO tipo_cliente(activo,codigo,descripcion,nombre) VALUES
+INSERT INTO tipo_cliente (activo,codigo,descripcion,nombre) VALUES
 (TRUE, 'REG', 'Cliente regular, no tiene beneficios especiales.', 'REGULAR'),
 (TRUE, 'EST', 'Cliente estandar, tiene beneficios basicos.', 'ESTANDAR'),
 (TRUE, 'PREF', 'Cliente preferencial, tiene beneficios preferenciales.', 'PREFERENCIAL'),
 (TRUE, 'VIP', 'Cliente regular, tiene beneficios especiales y atencion personalizada.', 'VIP');
+
+
+INSERT INTO tipo_cuenta (activo,codigo,descripcion,nombre) VALUES
+(TRUE,'AHO','Cuenta de ahorros para personas naturales.','AHORRO'),
+(TRUE,'COR','Cuenta corriente con sobregiro autorizado.','CORRIENTE'),
+(TRUE,'NOM','Cuenta para recepcion de salario.','NOMINA'),
+(TRUE,'PF','Cuenta de plazo fijo con rentabilidad garantizada :D.','PLAZO FIJO'),
+(TRUE,'JUV','Cuenta para menores de edad y jovenes.','JUVENIL'),
+(TRUE,'EMP','Cuenta para empresas y personas juridicas.','EMPRESARIAL'),
+(TRUE,'DIV','Cuenta en moneda extranjera.','DIVISA EXTRANJERA'),
+(TRUE,'DIG','Cuenta 101% digital sin papeleria.','DIGITAL');
