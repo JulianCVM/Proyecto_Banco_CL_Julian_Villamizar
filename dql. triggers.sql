ba04a1e1a91d9d95f549fd77ea1195f38bb9102a
Julián Camilo Villamizar Montañez <111820807+JulianCVM@users.noreply.github.com>
2025-06-30 22:32:38 -0500

    feat: :card_file_box: -- 2 Recalcular cuota al cambiar monto apertura

--- dql. triggers.sql
+++ dql. triggers.sql
@@ -25,3 +25,26 @@ SELECT * FROM pago_cuota_manejo WHERE cuota_manejo_id = 1;
 
 INSERT INTO pago_cuota_manejo (cuota_manejo_id,fecha_pago,metodo_pago_id,monto_pagado,pago_id) VALUES 
 (1,NOW()-INTERVAL 28 DAY,3,15000.00,4);
+
+
+
+-- 2 Recalcular cuota al cambiar monto apertura
+
+DROP TRIGGER IF EXISTS trg_recalcular_cuota_monto $$
+
+DELOIMITER $$
+
+CREATE TRIGGER 
+AFTER UPDATE ON cuotas_manejo
+FOR EACH ROW
+BEGIN
+
+    IF NEW.monto_apertura !+ OLD.monto_apertura THEN
+        UPDATE registro_cuota
+        SET monto_facturado = NEW.monto_apertura * 0.01
+        WHERE cuota_manejo_id = NEW.id;
+    END IF;
+
+END $$
+
+DELIMITER ;
